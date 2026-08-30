e56c0b5f22781f0077f4dd31ccdcfd55cef57378
MD ABDUL MUHIT <nadira04>
2026-08-31 01:17:11 +0600

    Fix Vercel asset path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/ShareMart/",
   plugins: [
     react(),
     tailwindcss(),
